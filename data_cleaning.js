bfe205f0f6912de647e53b5d4046cbfde75bceee
Kateka Seth <katekaseth@gmail.com>
2019-12-03 14:58:05 -0800

    add license count to cleaned data

--- data_cleaning.js
+++ data_cleaning.js
@@ -16,11 +16,21 @@ const csvWriter = createCsvWriter({
             id: 'storeCount',
             title: 'storeCount',
         },
+        {
+            id: 'licensedCount',
+            title: 'licensedCount'
+        },
+        {
+            id: 'companyCount',
+            title: 'companyCount'
+        },
     ]
 })
 
 let codeToState = {}
 let codeToCount = {}
+let codeToLicense = {}
+let codeToCompany = {}
 
 fs.createReadStream('states.csv')
     .pipe(csv())
@@ -34,6 +44,15 @@ fs.createReadStream('states.csv')
                 if (row.Country == "US") {
                     let storeCount = codeToCount[row['State/Province']]
                     storeCount ? codeToCount[row['State/Province']] = storeCount + 1 : codeToCount[row['State/Province']] = 1
+
+                    // count the different ownership types
+                    if (row["Ownership Type"] == "Licensed") {
+                        let licenseCount = codeToLicense[row['State/Province']]
+                        licenseCount ? codeToLicense[row['State/Province']] = licenseCount + 1 : codeToLicense[row['State/Province']] = 1
+                    } else if (row["Ownership Type"] == "Company Owned") {
+                        let companyCount = codeToCompany[row['State/Province']]
+                        companyCount ? codeToCompany[row['State/Province']] = companyCount + 1 : codeToCompany[row['State/Province']] = 1
+                    }
                 }
             })
             .on('end', () => {
@@ -41,10 +60,14 @@ fs.createReadStream('states.csv')
                 const keys = Object.keys(codeToCount)
                 keys.forEach(code => {
                     let count = codeToCount[code]
+                    let licenseCount = codeToLicense[code] ? codeToLicense[code] : 0
+                    let companyCount = codeToCompany[code] ? codeToCompany[code] : 0
                     let d = {
                         stateCode: code,
                         stateName: codeToState[code],
                         storeCount: count,
+                        licenseCount: licenseCount,
+                        companyCount: companyCount,
                     }
                     csvData.push(d)
                 })
@@ -54,89 +77,3 @@ fs.createReadStream('states.csv')
                     .then(() => console.log('The CSV file was written successfully'))
             })
     })
-
-
-// const csvWriter = createCsvWriter({
-//     path: 'starbucks_count_country.csv',
-//     header: [{
-//             id: 'code2',
-//             title: 'code2'
-//         },
-//         {
-//             id: 'code3',
-//             title: 'code3'
-//         },
-//         {
-//             id: 'count',
-//             title: 'count'
-//         },
-//         {
-//             id: 'licensedCount',
-//             title: 'licensedCount'
-//         },
-//         {
-//             id: 'companyCount',
-//             title: 'companyCount'
-//         },
-//         {
-//             id: 'otherCount',
-//             title: 'otherCount'
-//         },
-//     ]
-// });
-
-// let csvData = []
-// let codeToCount = {}
-// let codeToLicense = {}
-// let codeToCompany = {}
-// let codeToOther = {}
-// let code2ToCode3 = {}
-
-// fs.createReadStream('countries_codes_and_coordinates.csv')
-//     .pipe(csv())
-//     .on('data', (row) => {
-//         row['Alpha-2 code'] = row['Alpha-2 code'].trim().replace(/"/g, "")
-//         row['Alpha-3 code'] = row['Alpha-3 code'].trim().replace(/"/g, "")
-//         code2ToCode3[row['Alpha-2 code']] = row['Alpha-3 code']
-//     })
-//     .on('end', () => {
-//         fs.createReadStream('starbucks.csv')
-//             .pipe(csv())
-//             .on('data', (row) => {
-//                 let count = codeToCount[row.Country]
-//                 count ? codeToCount[row.Country] = count + 1 : codeToCount[row.Country] = 1
-                
-//                 if (row["Ownership Type"] == "Licensed") {
-//                     let licenseCount = codeToLicense[row.Country]
-//                     licenseCount ? codeToLicense[row.Country] = licenseCount + 1 : codeToLicense[row.Country] = 1
-//                 } else if (row["Ownership Type"] == "Company Owned") {
-//                     let companyCount = codeToCompany[row.Country]
-//                     companyCount ? codeToCompany[row.Country] = companyCount + 1 : codeToCompany[row.Country] = 1
-//                 } else {  // ownership type is other
-//                     let otherCount = codeToOther[row.Country]
-//                     otherCount ? codeToOther[row.Country] = otherCount + 1 : codeToOther[row.Country] = 1
-//                 }
-//             })
-//             .on('end', () => {
-//                 const keys = Object.keys(codeToCount)
-//                 keys.forEach((code => {
-//                     let count = codeToCount[code]
-//                     let licenseCount = codeToLicense[code] ? codeToLicense[code] : 0
-//                     let companyCount = codeToCompany[code] ? codeToCompany[code] : 0
-//                     let otherCount = codeToOther[code] ? codeToOther[code] : 0
-//                     let d = {
-//                         code2: code,
-//                         code3: code2ToCode3[code],
-//                         count: count,
-//                         licenseCount: licenseCount,
-//                         companyCount: companyCount,
-//                         otherCount: otherCount
-//                     }
-//                     csvData.push(d)
-//                 }))
-//                 console.log(csvData)
-//                 csvWriter
-//                     .writeRecords(csvData)
-//                     .then(() => console.log('The CSV file was written successfully'))
-//             })
-//     })
--- map.js
+++ map.js
@@ -6,18 +6,18 @@ var width = 1400;
 var height = 800;
 
 // D3 Projection
-var projection = d3.geo.albersUsa()
+var projection = d3.geoAlbersUsa()
     .translate([width / 2, height / 2])
     .scale([1400]);
 
 // Define path generator
-var path = d3.geo.path()
+var path = d3.geoPath()
     .projection(projection);
 
 // define color scale
-var colorScale = d3.scale.threshold()
-  .domain([0, 5, 10, 100, 300, 500])
-  .range(d3.schemeBlues[7]);
+var colorScale = d3.scaleThreshold()
+    .domain([0, 5, 10, 100, 300, 500])
+    .range(d3.schemeBlues[7]);
 
 //Create SVG element and append map to the SVG
 var svg = d3.select("body")
@@ -31,10 +31,14 @@ let div = d3.select("body")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
-let toolChart = div.append('svg')
-    .attr('width', 300)
-    .attr('height', 300)
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    tt_width = 960 - margin.left - margin.right,
+    tt_height = 500 - margin.top - margin.bottom;
 
+let toolChart = div.append("svg")
+    .attr("width", tt_width + margin.left + margin.right)
+    .attr("height", tt_height + margin.top + margin.bottom)
 
 // Load GeoJSON of the US states
 d3.json("https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json", function (json) {
@@ -48,32 +52,66 @@ d3.json("https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/g
         .style("stroke-width", "1")
 
     // load in starbucks store count
-    d3.csv("usa_starbucks_count.csv", function(d) {
+    d3.csv("usa_starbucks_count.csv", function (d) {
         usaData = d
         let nameToCount = {}
         usaData.forEach(element => {
             nameToCount[element.stateName] = parseInt(element.storeCount)
         });
-        console.log(nameToCount)
         // fill with color scaled to number of store
-        map.attr("fill", function(d) {
-            stateName = d.properties.name
-            return colorScale(nameToCount[stateName]);
-        })
-        .on("mouseover", (d) => {
-            toolChart.selectAll("*").remove()
-            div.transition()
-                .duration(200)
-                .style("opacity", .9);
-            div.html("Number of Stores" + "</br>" + d.properties.name + ": " + nameToCount[d.properties.name])
-                .style("left", (d3.event.pageX) + "px")		
-                .style("top", (d3.event.pageY - 28) + "px");	
-            // plotLicense(d.id)
-        })
-        .on("mouseout", (d) => {
-            div.transition()
-                .duration(500)
-                .style("opacity", 0);
-        });
+        map.attr("fill", function (d) {
+                stateName = d.properties.name
+                return colorScale(nameToCount[stateName]);
+            })
+            .on("mouseover", (d) => {
+                toolChart.selectAll("*").remove()
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                plotLicense(d.properties.name)
+                // div.html("Number of Stores" + "</br>" + d.properties.name + ": " + nameToCount[d.properties.name])
+                //     .style("left", (d3.event.pageX) + "px")		
+                //     .style("top", (d3.event.pageY - 28) + "px");	
+                
+            })
+            .on("mouseout", (d) => {
+                div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
     })
 })
+
+function plotLicense(stateName) {
+    var x = d3.scaleBand()
+        .range([0, tt_width])
+        .padding(0.1);
+    var y = d3.scaleLinear()
+        .range([tt_height, 0]);
+
+    x.domain(usaData.map(function (d) {
+        return d.stateName;
+    }));
+    y.domain([0, d3.max(usaData, function (d) {
+        return parseInt(d.storeCount);
+    })]);
+
+    // append the rectangles for the bar chart
+    toolChart.selectAll(".bar")
+        .data(usaData[stateName])
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("fill", function(d) { console.log(d); })
+        // .attr("width", x.bandwidth())
+        // .attr("y", function(d) { return y(d.sales); })
+        // .attr("height", function(d) { return height - y(d.sales); });
+
+    // add the x Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // add the y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
+}
