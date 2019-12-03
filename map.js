aea1dd246b1f223850088c941a397e16876f34ff
Kateka Seth <katekaseth@gmail.com>
2019-12-03 11:34:49 -0800

    add simple tooltip

--- map.js
+++ map.js
@@ -3,6 +3,15 @@ var width = 1000
 var height = 800
 var svg = d3.select("body").append("svg").attr("width", width).attr("height", height)
 
+// make tooltip
+let div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+let toolChart = div.append('svg')
+    .attr('width', 300)
+    .attr('height', 300)
+
 // Map and projection
 var path = d3.geo.path();
 var projection = d3.geo.mercator()
@@ -36,5 +45,28 @@ d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/w
                 d.total = codeToCount[d.id] || 0;
                 return colorScale(d.total);
             })
+            .on("mouseover", (d) => {
+                toolChart.selectAll("*").remove()
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                div.html(d.id + ": " + formatCount(codeToCount[d.id]))	
+                    .style("left", (d3.event.pageX) + "px")		
+                    .style("top", (d3.event.pageY - 28) + "px");	
+                // plotPopulation(d.country, toolChart)
+                // div
+                //     .style("left", (d3.event.pageX) + "px")
+                //     .style("top", (d3.event.pageY - 28) + "px");
+                
+            })
+            .on("mouseout", (d) => {
+                div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
     })
 })
+
+function formatCount(count) {
+    return count ? count : "0"
+}
